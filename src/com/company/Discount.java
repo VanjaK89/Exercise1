e0ed752e94390041b577ba20c6123ad8dd282168
Kavgić Vanja <s51637@edu.campus02.at>
2022-03-02 15:48:28 +0100

    added txt

---
+++ src/com/company/Discount.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Discount {
+    public static void main(String[] args) {
+
+        // write your code here
+        boolean seniorCitizen = true;
+        boolean dog = false;
+        boolean student = false;
+        System.out.println("On this bus you get a discount: " + (seniorCitizen || dog || student ));
+
+
+        int nonFiction = 4;
+        int fiction = 2;
+        System.out.println("In this bookshop I get a discount: " + (nonFiction >= 2 && fiction > 0));
+
+        int nonAlcoholic = 1;
+        int alcoholic = 0;
+        System.out.println("In this shop I get non alcoholic drink: " + (nonAlcoholic >= 1 && alcoholic <1) );
+
+
+
+
+
+
+
+
+    }
+}
