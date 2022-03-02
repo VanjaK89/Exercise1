9944639b5b530418f9ec3be053125496d09b8408
Kavgić Vanja <s51637@edu.campus02.at>
2022-03-02 13:45:07 +0100

    Initial commit

---
+++ src/com/company/Names.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Names {
+
+    public static void main(String[] args) {
+	// write your code here
+
+      String name = "Jane";
+      String surname = "Doe";
+        System.out.println("The full name is " + name + " " + surname + ".");
+        
+
+
+
+    }
+}
